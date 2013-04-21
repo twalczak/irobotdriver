1efe2d90b09f6f4785a45f9d8e8bf010036a1f1a
tomasztik <twalczak@twalczak-MacBookAir.(none)>
2013-04-21 17:42:58 -0400

    Continue work on monitor: not ready

--- monitor/monitor.cpp
+++ monitor/monitor.cpp
@@ -20,13 +20,15 @@ monitor::monitor(QWidget *parent) : QWidget(parent, Qt::FramelessWindowHint)
 	timer->start(); running=true;
 	X=100;
 	Y=100;
+
+	robot1.start();
 }
 
 void monitor::paintEvent(QPaintEvent *event)
 {	
 	QFont font("Lucida Console");
 	font.setPixelSize(fontSize);
-	font.setStyleStrategy(QFont::NoAntialias);
+	//font.setStyleStrategy(QFont::NoAntialias);
 	
 	//QColor c(255,255,255,255);
 	
@@ -39,7 +41,8 @@ void monitor::paintEvent(QPaintEvent *event)
 	p.drawRect(0,0,width(),height());
 	
 	p.setPen(QPen(Qt::white, 1));						
-	p.drawPolygon(points, 4);
+	//p.drawPolygon(points, 4);
+	p.drawRect(100,100,10,10);
 	
 	p.drawText(3,fontSize, "ROBOT1: OFFLINE");
 	p.drawText(3,fontSize*2, "  192.168.1.101: No response...");
@@ -61,7 +64,6 @@ void monitor::timerTimeout()
 	monitor::points[1] = QPointF(X-(sin(theta+(0.75*M_PI))*shipSize),Y-(cos(theta+(M_PI*0.75))*shipSize));
 	monitor::points[2] = QPointF(X-(sin(theta)*shipSize*2),Y-(cos(theta)*2*shipSize));
 	monitor::points[3] = QPointF(X-(sin(theta+(5*M_PI)/4)*shipSize),Y-(cos(theta+(5*M_PI)/4)*shipSize));
-
 	
 	//REDRAW WINDOW
 	this->update();
@@ -84,3 +86,18 @@ void monitor::keyReleaseEvent(QKeyEvent *event)
 	if((event->key()==Qt::Key_Right)) right = false;
 	if((event->key()==Qt::Key_Space)) shoot = false;
 }
+
+
+
+/*-----------------------------------------------------------------
+						ROBOT MEMBER FUNCTIONS
+-----------------------------------------------------------------*/
+
+void* Robot::network_thread(void) {
+	while(1) { usleep(500000);
+		std::cout << "RUNNING\n";
+	}
+	return 0;
+}
+
+
--- monitor/monitor.h
+++ monitor/monitor.h
@@ -2,6 +2,26 @@
 #define MONITOR_H
 
 #include <QWidget>
+#include <iostream>
+#include <pthread.h>
+#include <unistd.h>
+
+
+class Robot  {
+public:
+	Robot(){
+	};
+	static void*  call_network_thread(void *arg) { return ((Robot*)arg)->network_thread(); }
+	void* network_thread(void);
+	void start(void){
+		pthread_t tid;
+		int result;
+		result = pthread_create(&tid, 0, Robot::call_network_thread, this);
+		if (result == 0)
+			pthread_detach(tid);
+	};
+};
+
 
 class monitor : public QWidget
 {
@@ -9,7 +29,8 @@ class monitor : public QWidget
 
 public:
 	monitor(QWidget *parent = 0);
-	
+	Robot robot1;
+
 protected:
 	void paintEvent(QPaintEvent *event);
 	void keyPressEvent(QKeyEvent *event);
@@ -18,7 +39,6 @@ protected:
 private slots:
 	void timerTimeout();
 
-	
 private:
 	//KEYPRESS
 	bool left, right, accel, shoot;
